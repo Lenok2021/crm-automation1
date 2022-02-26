d4d0f9c092fea48b8d57e9b2d9de1c98d66e7eff
cross <crosstown11223@yahoo.com>
2022-02-26 11:56:20 -0500

    login with valid credentials

--- src/test/java/com/nextbasecrm/tests/LogingFuction.java
+++ src/test/java/com/nextbasecrm/tests/LogingFuction.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -54,7 +55,11 @@ public class LogingFuction {
         loginButton.click();
         //loginButton+ Keys.ENTER;
 
+        //  verify title
+        String expectedTitle = "Portal";
+        String actualTitle = driver.getTitle();
 
+        Assert.assertEquals(actualTitle,expectedTitle);
 
 
     }
