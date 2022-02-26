affba6a191bc96f85f9b6694a7ed9d0b0f449c06
cross <crosstown11223@yahoo.com>
2022-02-26 11:43:47 -0500

    User story is done

--- src/test/java/com/nextbasecrm/tests/LogingFuction.java
+++ src/test/java/com/nextbasecrm/tests/LogingFuction.java
@@ -2,9 +2,14 @@ package com.nextbasecrm.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.BrowserUtils;
 import utilities.ConfigurationReader;
 
 import java.util.concurrent.TimeUnit;
@@ -28,11 +33,18 @@ public class LogingFuction {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // go to login page
         driver.get(ConfigurationReader.getProperty("env"));
 
     }
 
 
 
+    @Test
+    public void  login_with_valid_credentials_with_login_btn() {
+       //username
+        WebElement username = driver.findElement(By.xpath("(//input[@class = 'login-inp'])[1]"));
 
+
+    }
 }
